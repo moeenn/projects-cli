f48076524239b0e30982f6aa7510b98b9c3304a4
Muhammad Moeen <muhammad.moeen@themoonshotfactory.com>
2025-03-20 21:03:23 +0500

    update plain js project template

--- internal/templates/js/js.go
+++ internal/templates/js/js.go
@@ -9,11 +9,12 @@ import (
 func NewJSConfig(args *templates.TemplateArgs) *templates.TemplateConfig {
 	mainSrc := filepath.Join(args.RootPath, "src")
 	files := map[string]string{
-		"js.index_mjs":      filepath.Join(mainSrc, "index.mjs"),
-		"js.index_test_mjs": filepath.Join(mainSrc, "index.test.mjs"),
-		"js.jsconfig_json":  filepath.Join(args.RootPath, "jsconfig.json"),
-		"js.package_json":   filepath.Join(args.RootPath, "package.json"),
-		"js.readme_md":      filepath.Join(args.RootPath, "README.md"),
+		"js.index_mjs":        filepath.Join(mainSrc, "index.mjs"),
+		"js.index_test_mjs":   filepath.Join(mainSrc, "index.test.mjs"),
+		"js.jsconfig_json":    filepath.Join(args.RootPath, "jsconfig.json"),
+		"js.package_json":     filepath.Join(args.RootPath, "package.json"),
+		"js.readme_md":        filepath.Join(args.RootPath, "README.md"),
+		"js.eslint_config_js": filepath.Join(args.RootPath, "eslint.config.js"),
 	}
 
 	return &templates.TemplateConfig{
